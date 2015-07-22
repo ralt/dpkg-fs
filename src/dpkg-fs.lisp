986ea403e1a0788fef3e23090fd18b22f87908d5
Florian MARGAINE <florian@margaine.com>
2015-07-23 01:50:29 +0200

    Starts the architecture to get dir content.

--- src/dpkg-fs.lisp
+++ src/dpkg-fs.lisp
@@ -5,6 +5,9 @@
 
 (annot:enable-annot-syntax)
 
+(defun cat (&rest args)
+  (apply #'concatenate 'string args))
+
 @export
 (defun disable-debugger ()
   (labels
@@ -14,13 +17,46 @@
          (sb-ext:exit)))
     (setf *debugger-hook* #'exit)))
 
-(defun directory-content (split-path)
-  (log:debug "directory-content: ~A" split-path)
-  (get-packages))
+(defmethod dir-content (path (type (eql :root)))
+  (unless path
+    (return-from dir-content '("installed" "remote" "sync")))
+  (let ((folder (first path)))
+    (cond ((string= folder "installed") (dir-content (rest path) :installed))
+          ((string= folder "remote") (dir-content (rest path) :remote)))))
 
-(defun is-directory (split-path)
-  (log:debug "is-directory: ~A" split-path)
+(defmethod dir-content (path (type (eql :installed)))
+  (unless path
+    (return-from dir-content (installed-packages)))
+  (when (package-exists (first path))
+    (dir-content (rest path) :package-info)))
+
+(defmethod dir-content (path (type (eql :package-info)))
+  (unless path
+    (return-from dir-content '("name" "version" "desc"))))
+
+(defmethod dir-content (path (type (eql :remote))))
+
+(defun directory-content (split-path &optional (type :root))
+  (log:debug "directory-content: ~A" split-path)
+  (dir-content split-path type))
+
+(defun directoryp (split-path)
+  (log:debug "directoryp: ~A" split-path)
+  (unless split-path
+    (return-from directoryp t))
+  (not (string= (first split-path) "sync")))
+
+(defun file-read (split-path)
+  (log:debug "file-read: ~A" split-path)
   t)
+
+(defun file-size (split-path)
+  (log:debug "file-size: ~A" split-path)
+  0)
+
+(defun symlink-target (split-path)
+  (log:debug "symlink-target: ~A" split-path)
+  "")
 
 @export
 (defun main (args)
@@ -28,11 +64,19 @@
   (log:debug "fuse-run")
   (cl-fuse:fuse-run `("none" ,(second args) "-d")
                     :directory-content 'directory-content
-                    :directoryp 'is-directory))
+                    :directoryp 'directoryp
+                    :file-read 'file-read
+                    :file-size 'file-size
+                    :symlink-target 'symlink-target))
 
-
-(defun get-packages ()
+(defun installed-packages ()
   (cl-ppcre:split " "
                   (let ((s (make-string-output-stream)))
                     (uiop:run-program "dpkg-query --showformat='${Package} ' --show" :output s)
                     (get-output-stream-string s))))
+
+(defun package-exists (name)
+  (string= "install ok installed"
+           (let ((s (make-string-output-stream)))
+             (uiop:run-program (cat "dpkg-query --showformat='${Status}' --show " name) :output s :ignore-error-status t)
+             (get-output-stream-string s))))
